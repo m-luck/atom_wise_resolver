a55f6f1cc4bda42663d8884c6acc9d9c8a14eed4
Ashkara Vingh II <michael.j.lukiman@gmail.com>
2019-04-01 15:59:12 -0400

    Go time!

--- resolve.py
+++ resolve.py
@@ -1,17 +1,17 @@
 from sub_davis_putnam_solver import *
 import sys
 
-if len(sys.argv) == 1:
+if len(sys.argv) == 1: # If there's an argument take it (independent of memory_cycles)
     clauses = p.read_in()
 else:
-    clauses = p.read_in(sys.argv[1])
+    clauses = p.read_in(sys.argv[1]) # Else we're in a pipeline from the memory_cycle frontend (and we know where the clauses should be)
 decisions = {}
-lits = enumerate_literals(clauses)
-res = DPLLmod(clauses, decisions, lits)
+lits = enumerate_literals(clauses) # Keep track of literals.
+res = DPLLmod(clauses, decisions, lits) # Run resolution!
 resString = []
-if res != None:
+if res != None: # If there is an answer..
     res = dict(sorted(res.items()))
-    for clause in res:
+    for clause in res: # Convert it to ATOM T|F format
         boolStr = 'T' if res[clause] == True else 'F'
         clauseStr = str(clause) + ' ' + boolStr
         resString.append(clauseStr)
@@ -20,7 +20,7 @@ resString.append('0')
 output = ''
 for string in resString:
     output += string + '\n'
-output += '---' + str(p.read_glossary())
+output += '---' + str(p.read_glossary()) # Pass on the backmatter.
 
 with open('decisions','w+') as f:
-    f.write(str(output))
+    f.write(str(output)) # File for the backend to read. 
